8e572598af882d761c3e21955fe410e17485f08d
Peter <kulaspa@gmail.com>
2016-03-11 08:39:28 +0700

    Change dialog design according to Google's dialog design

--- app/src/main/java/com/ske/minitrello/activities/ShowCardInfoActivity.java
+++ app/src/main/java/com/ske/minitrello/activities/ShowCardInfoActivity.java
@@ -1,19 +1,14 @@
 package com.ske.minitrello.activities;
 
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.ske.minitrello.R;
 import com.ske.minitrello.views.adapters.CommentAdapter;
@@ -22,17 +17,9 @@ import com.ske.minitrello.models.CardKeeper;
 import com.ske.minitrello.models.CardList;
 import com.ske.minitrello.models.Comment;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
 public class ShowCardInfoActivity extends AppCompatActivity {
-
-    TextView cardTitle;
-    TextView cardDescription;
-    ImageView editTitleBtn;
-    ImageView editDescBtn;
-    ImageView addCommentBtn;
 
     RecyclerView recyclerView;
     CommentAdapter commentAdapter;
@@ -57,39 +44,6 @@ public class ShowCardInfoActivity extends AppCompatActivity {
         }
 
         //setCustomActionBar();
-
-
-//
-//        recyclerView = (RecyclerView) findViewById(R.id.comment_recycler_view);
-//        LinearLayoutManager llm = new LinearLayoutManager(this);
-//
-//        comments = card.getComments();
-//        commentAdapter = new CommentAdapter(comments);
-//
-//        recyclerView.setLayoutManager(llm);
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setAdapter(commentAdapter);
-//        recyclerView.setItemAnimator(new DefaultItemAnimator());
-//
-
-
-//
-//        cardTitle.setFocusableInTouchMode(false);
-//        cardTitle.setFocusable(false);
-//        cardTitle.setCursorVisible(false);
-//        cardTitle.setBackgroundColor(Color.TRANSPARENT);
-//
-//        addCommentBtn = (ImageView) findViewById(R.id.add_comment_button);
-//        addCommentBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Comment comment = new Comment("New comment");
-//                card.addComment(comment);
-//                commentAdapter.notifyItemInserted(comments.size() - 1);
-//            }
-//        });
-//
-
 
 
     }
